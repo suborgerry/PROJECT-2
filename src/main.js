f87e03b5eebb204a293c4f301613e4908803ce99
borgerry <gerryborodin@gmail.com>
2022-11-09 16:20:39 +0200

    Init with CLI

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * imports
+ */
+import './scss/main.scss';
+// import Cookies from 'js-cookie';
+import './js/common.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.console.log('Hello from main.js 👋.');
+});
